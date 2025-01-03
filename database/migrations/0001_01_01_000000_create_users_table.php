39a1d23fe2b024ff336bbeefcdc4b0531fa3ae43
tuanba <tuanbui.10103@gmail.com>
2025-01-03 15:09:27 +0700

    update: add final new line

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('address');
             $table->string('phone_number');
             $table->string('email')->unique();
-            $table->string('role');
+            $table->string('role'); // 1 2 3
             $table->string('image')->nullable();
             $table->boolean('status')->default(true);
             $table->timestamps();
