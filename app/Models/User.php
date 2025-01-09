1cb77fc3376030a2f0dc9f8f2e6f489a66b8d6c1
tuanba <tuanbui.10103@gmail.com>
2025-01-10 00:56:13 +0700

    update: update model user funtion isAdmin for login

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+
 
 class User extends Authenticatable
 {
@@ -57,8 +59,10 @@ class User extends Authenticatable
         return $this->hasMany(Video::class, 'teacher_id');
     }
 
-    public function getIsAdminAttribute(): bool
+    protected function isAdmin(): Attribute
     {
-        return $this->role === 'admin' && $this->status === 1;
+        return Attribute::make(
+            get: fn() => $this->role === 'admin' && $this->status === 1
+        );
     }
 }
