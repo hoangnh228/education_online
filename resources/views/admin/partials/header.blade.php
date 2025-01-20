59367d2868b01b4cdca857a3a512c69bb655a0d8
tuanba <tuanbui.10103@gmail.com>
2025-01-21 01:39:43 +0700

    update: update logout for client

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -16,6 +16,9 @@
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('admin.videos') }}">Video</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('admin.categories') }}">Category</a>
+                </li>
             </ul>
             <form action="{{ route('admin.logout') }}" method="POST" class="d-flex">
                 @csrf
