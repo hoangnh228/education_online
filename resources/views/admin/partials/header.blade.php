308dee637be3a62d7307506e4cfeb4559fbb67b7
tuanba <tuanbui.10103@gmail.com>
2025-01-21 14:22:26 +0700

    update: update header admin

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -8,22 +8,39 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav me-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('admin.users') }}">User</a>
+                    <a class="nav-link" href="{{ route('admin.users') }}">Users</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('admin.courses') }}">Courses</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('admin.videos') }}">Video</a>
+                    <a class="nav-link" href="{{ route('admin.videos') }}">Videos</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('admin.categories') }}">Category</a>
+                    <a class="nav-link" href="{{ route('admin.categories') }}">Categories</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('admin.categories') }}">Review</a>
                 </li>
             </ul>
-            <form action="{{ route('admin.logout') }}" method="POST" class="d-flex">
-                @csrf
-                <button type="submit" class="btn btn-outline-danger">Logout</button>
-            </form>
+        </div>
+
+        <div class="dropdown">
+            <a class="nav-link dropdown-toggle d-flex align-items-center" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <img src="{{ Auth::user()->image ? asset('storage/' . Auth::user()->image) : asset('default-avatar.png') }}" 
+                     alt="Avatar" class="rounded-circle me-2" width="30" height="30">
+                <span>{{ Auth::user()->user_name }}</span>
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item text-primary" href="{{ route('admin.settings') }}">Change Settings</a></li>
+                <li>
+                    <form action="{{ route('admin.logout') }}" method="POST">
+                        @csrf
+                        <button class="dropdown-item text-danger" type="submit">Logout</button>
+                    </form>
+                </li>
+            </ul>
+        </li>
         </div>
     </div>
 </nav>
